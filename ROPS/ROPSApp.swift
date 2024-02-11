fa444a3d44b7d5405ea6d6a4cde3f3cab064da78
Görkem Güray <grkmgry@gmail.com>
2024-02-11 21:34:37 +0300

    Initial Commit

---
+++ ROPS/ROPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROPSApp.swift
+//  ROPS
+//
+//  Created by Görkem Güray on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ROPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
